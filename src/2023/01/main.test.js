41bcf720834ba182dd035b8675097feb67be249d
Assad Khan <assadkhan@outlook.com>
2023-12-02 11:23:17 +0000

    Add test scripts so that tests can be performed by year

--- src/2023/01/main.test.js
+++ src/2023/01/main.test.js
@@ -1,15 +1,17 @@
 import { part1, part2 } from './main';
 import { input } from './input';
 
-describe('Day 1', () => {
-  test('Part 1', () => {
-    const result = part1(input);
-    expect(result).toBe(54239);
-  });
+describe('2023', () => {
+  describe('Day 1', () => {
+    test('Part 1', () => {
+      const result = part1(input);
+      expect(result).toBe(54239);
+    });
 
-  test('Part 2', () => {
-    const result = part2(input);
-    console.log(result); // Display console output
-    // expect(result).toBe(206104);
+    test('Part 2', () => {
+      const result = part2(input);
+      console.log(result); // Display console output
+      // expect(result).toBe(206104);
+    });
   });
 });
